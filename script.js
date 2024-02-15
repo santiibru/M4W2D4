9f1069439fece3f51d4d2f5e9e9f4562222ff35c
Santiago Bruno <santiago.p.bruno@gmail.com>
2024-02-15 18:31:55 +0100

    commit

--- script.js
+++ script.js
@@ -4,37 +4,46 @@ const inputSearch = document.getElementById("searchField");
 //BUTTON RICERCA
 const btnSearch = document.getElementById("searchBtn");
 
+// BUTTON AGGIUNGERE AL CARRELLO
+const btnAdd = document.getElementsByClassName("btn-add")
+
+// CARRELLO RISULTATI DA APPENDERE
+
+const cartArea = document.getElementById("offcanvas-body")
+
 //CARD RISULTATI DA APPENDERE
 const cardArea = document.getElementById("row");
 
+//RISULTATI ATTIVI
+let activeResults;
 
+//window.location.html href
+//html nuovo, con tutto il layout, collegare le pagine e poi fare il fetch
 
+window.onload = () => {
+    getBook()
+}
 
 function getBook() {
     fetch("https://striveschool-api.herokuapp.com/books")
         .then((response) => response.json())
         .then((result) => cycleRes(result))
-        .then((data) => searchBook(data))
-        .catch((err) => console.error(err));
+        .catch(err => console.error(err));
 }
-getBook()
 
 
-function cycleRes(data) {
-    data.map((element) => {
+
+function cycleRes(res, savedResults = true) {   
+    if (savedResults) {
+        activeResults = res;
+    }
+    cardArea.innerHTML = "";
+    res.forEach((element) => {
         createCards(element);
-        return data
+
     });
 }
 
-// function searchBook(data) {
-//     let searchValue = inputSearch.value;
-//     const findBook = data.filter((elem) => elem.title);
-//     if (findBook !== searchValue) {
-//         createCards
-//     }
-    
-//}
 
 
 function createCards(book) {
@@ -53,11 +62,25 @@ function createCards(book) {
     let cardTitle = document.createElement("h5");
     cardTitle.classList.add("card-title");
     cardTitle.innerText = book.title;
-    let priceButton = document.createElement("a");
-    priceButton.classList.add("btn", "btn-outline-secondary", "ms-3");
-    priceButton.innerText = ("Skip");
+    let cardPrice = document.createElement("h6");
+    cardPrice.classList.add("card-subtitle", "text-center", "mt-2","mb-2");
+    cardPrice.innerText = ("$") + book.price;
+    let buttonsCointainer = document.createElement("div");
+    buttonsCointainer.classList.add("d-flex", "justify-content-around");
+    let skipButton = document.createElement("a");
+    skipButton.classList.add("btn", "btn-outline-danger");
+    skipButton.innerText = ("X");
+    let detailsButton = document.createElement("a")
+    detailsButton.classList.add("btn", "btn-outline-secondary", "px-3");
+    detailsButton.innerText = ("See more")
     let cartButton = document.createElement("a");
-    cartButton.classList.add("btn", "btn-outline-success", "ms-5");
+    cartButton.classList.add("btn", "btn-outline-success");
+    cartButton.role = ("button");
+    cartButton.tabIndex = ("0")
+    cartButton.setAttribute("data-bs-toggle", "popover");
+    cartButton.setAttribute("data-bs-trigger", "focus");
+    cartButton.setAttribute("data-bs-placement", "top");
+    cartButton.setAttribute("data-bs-content", "Book added to cart successfully!");
     cartButton.innerHTML = `<i class="fa-solid fa-cart-plus"></i>`
 
     cardArea.appendChild(cardsCol);
@@ -65,33 +88,75 @@ function createCards(book) {
     cards.appendChild(covers);
     cards.appendChild(cardBody);
     cardBody.appendChild(cardTitle);
-    cardBody.appendChild(priceButton);
-    cardBody.appendChild(cartButton);
+    cardBody.appendChild(cardPrice);
+    cardBody.appendChild(buttonsCointainer);
+    buttonsCointainer.appendChild(skipButton);
+    buttonsCointainer.appendChild(detailsButton);
+    buttonsCointainer.appendChild(cartButton);
+
+    //INIZIALIZO POPOVER
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+    
+    //AGGIUNGO AL CARRELLO
+    cartButton.addEventListener("click", () => {
+    addToCart(book)
+    })
+
+    //ELIMINO LA CARD CHE NON VOGLIO
+    skipButton.addEventListener("click", () => {
+        skipBook(book)
+    }) 
+
+    function skipBook() {
+        cardsCol.classList.add("d-none");
+    }
 
 }
 
-// function searchBook(resArray) {
-//     //let searchValue = inputSearch.value;
-//     [...resArray].filter((element) => console.log(element.title));
-   
 
 
 
-// btnSearch.addEventListener('click', (event) => {
-//     let searchValue = inputSearch.value;
-//     filteredBooks = book.title.filter((element) => element === searchValue)
+
+function searchBook() {
+    if (activeResults) {
+        let searchValue = inputSearch.value;
+        let findBook = activeResults.filter((elem) => {
+            return elem.title.toLowerCase().includes(searchValue.toLowerCase().trim());
+        });
+        cycleRes(findBook, false);
+    }
+}
+
+function addToCart(element) {
+    let titleAdded = document.createElement("h5")
+    titleAdded.classList.add("booksAdded-title")
+    titleAdded.innerText = element.title
+    let cartContainer = document.createElement("div")
+    cartContainer.classList.add("d-flex", "justify-content-between", "align-items-center")
+    let priceAdded = document.createElement("h6")
+    priceAdded.classList.add("booksAdded-price", "text-center", "fs-5")
+    priceAdded.innerText = ("$") + element.price
+    let photoAdded = document.createElement("img")
+    photoAdded.classList.add("booksAdded-img")
+    photoAdded.src = element.img
+    let hr = document.createElement("hr")
+    hr.classList.add("hr")
+    let cartEmpty = document.getElementById("cart-empty")
+    cartEmpty.classList.add("d-none")
+    let checkoutBtn = document.getElementById("checkout-btn")
+    checkoutBtn.classList.remove("d-none")
+    let totalPrice = document.getElementById("total-price")
+    totalPrice.classList.remove("d-none")
+    totalPrice.innerText = ("Total $") + (Number(totalPrice.innerText) + Number(element.price))
     
-//     console.log(filteredBooks)
-// })
-
-
-
-
-
-        
-        
     
-       
-
+    cartArea.appendChild(titleAdded)
+    cartArea.appendChild(cartContainer)
+    cartContainer.appendChild(photoAdded)
+    cartContainer.appendChild(priceAdded)
+    cartArea.appendChild(hr)
+    
+    }
 
 
